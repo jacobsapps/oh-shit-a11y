996fce170fb4c28a79360a38bb6822d9d72fc4e6
Jacob Bartlett <jacob@Jacobs-MacBook-Pro.local>
2024-02-25 14:06:09 +0000

    Initial commit

---
+++ OhShitA11y/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OhShitA11y
+//
+//  Created by Jacob Bartlett on 25/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OhShitA11y/OhShitA11yApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhShitA11yApp.swift
+//  OhShitA11y
+//
+//  Created by Jacob Bartlett on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct OhShitA11yApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
